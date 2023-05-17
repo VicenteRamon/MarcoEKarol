9329371b56d0c5c4b8a068f39cf984269df1f8ce
Ramon Vicente <vicente.silva.ramon@gmail.com>
2023-05-17 20:21:16 -0300

    Configure to use Sass

--- MarcoEKarol/wwwroot/js/site.js
+++ MarcoEKarol/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    function showSlide(slideIndex) {
+        slides.forEach((slide, index) => {
+            if (index === slideIndex) {
+                slide.classList.add("active");
+            } else {
+                slide.classList.remove("active");
+            }
+        });
+    }
+
+    setInterval(() => {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }, 7000);
+});
